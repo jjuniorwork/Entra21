9bb6172cc6d7753edcbbf567013100bbe38a4ae7
Jose Entra21 <jjuniorwork@gmail.com>
2021-06-23 20:02:09 -0300

    nova att

---
+++ Entra21Noturno-main/MeuPrimeiroForm/MeuPrimeiroForm/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace MeuPrimeiroForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnMultiplicacao = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Location = new System.Drawing.Point(12, 254);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(180, 23);
+            this.btnSoma.TabIndex = 0;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(71, 146);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(179, 23);
+            this.txtNumero1.TabIndex = 1;
+            this.txtNumero1.Leave += new System.EventHandler(this.txtNumero1_Leave);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Location = new System.Drawing.Point(217, 254);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(180, 23);
+            this.btnSubtracao.TabIndex = 2;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_click);
+            // 
+            // btnMultiplicacao
+            // 
+            this.btnMultiplicacao.Location = new System.Drawing.Point(419, 254);
+            this.btnMultiplicacao.Name = "btnMultiplicacao";
+            this.btnMultiplicacao.Size = new System.Drawing.Size(180, 23);
+            this.btnMultiplicacao.TabIndex = 3;
+            this.btnMultiplicacao.Text = "x";
+            this.btnMultiplicacao.UseVisualStyleBackColor = true;
+            this.btnMultiplicacao.Click += new System.EventHandler(this.btnMultiplicacao_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Location = new System.Drawing.Point(618, 254);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(180, 23);
+            this.btnDivisao.TabIndex = 4;
+            this.btnDivisao.Text = "\\";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(420, 146);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(179, 23);
+            this.txtNumero2.TabIndex = 5;
+            this.txtNumero2.Leave += new System.EventHandler(this.txtNumero2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(419, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Número 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnMultiplicacao);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.btnSoma);
+            this.Name = "Form1";
+            this.Text = "Bruno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnMultiplicacao;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDivisao;
+    }
+}
+
---
+++ Entra21Noturno-main/MeuPrimeiroForm/MeuPrimeiroForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeuPrimeiroForm
+{
+    public partial class Form1 : Form
+    {
+        int numero1, numero2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{numero1 + numero2}");
+        }
+
+        private void btnSubtracao_click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{numero1 - numero2}");
+        }
+
+        private void txtNumero2_Leave(object sender, EventArgs e)
+        {
+            numero2 = int.Parse(txtNumero2.Text);
+        }
+
+        private void txtNumero1_Leave(object sender, EventArgs e)
+        {
+            numero1 = int.Parse(txtNumero1.Text);
+        }
+
+        private void btnMultiplicacao_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{numero1 * numero2}");
+        }
+    }
+}
